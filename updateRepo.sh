a10e31d1396416f0d29184e01d05cb30e803692b
Stanislav Ch. Nikolov <stanislav@cukii.tk>
2016-10-21 11:59:30 +0300

    2016-10-21

--- updateRepo.sh
+++ updateRepo.sh
@@ -4,5 +4,14 @@ cp ~/.config/nvim/init.vim ./init.vim
 cp ~/.config/zsh/zshrc ./zshrc
 cp ~/.xinitrc ./xinitrc
 
-diff ./hax ~/.hax -q > /dev/null || (git rm -r ./hax && cp -r ~/.hax ./hax && git add ./hax)
-diff ./i3 ~/.config/i3 -q > /dev/null || (git rm -r ./i3 && cp -r ~/.config/i3 ./i3 && git add ./i3)
+diff --brief -Nr ./hax ~/.hax > /dev/null \
+	|| (git rm -r ./hax; rm -rf ./hax; cp -r ~/.hax ./hax; git add ./hax)
+
+diff --brief -Nr ./i3 ~/.config/i3 > /dev/null \
+	|| (git rm -r ./i3; rm -rf ./i3; cp -r ~/.config/i3 ./i3; git add ./i3)
+
+diff --brief -Nr ./bspwm ~/.config/bspwm > /dev/null \
+	|| (git rm -r ./bspwm; rm -rf ./bspwm; cp -r ~/.config/bspwm ./bspwm; git add ./bspwm)
+
+diff --brief -Nr ./sxhkd ~/.config/sxhkd > /dev/null \
+	|| (git rm -r ./sxhkd; rm -rf ./sxhkd; cp -r ~/.config/sxhkd ./sxhkd; git add ./sxhkd)
