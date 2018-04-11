35d6af6e8e10b15b2738b9b09de8fbc96cc571a7
Stanislav Ch. Nikolov <stanislav@cukii.me>
2018-04-11 18:52:44 +0300

    new updateRepo.sh

--- updateRepo.sh
+++ updateRepo.sh
@@ -3,6 +3,7 @@
 cp ~/.config/nvim/init.vim ./init.vim
 cp ~/.config/zsh/zshrc ./zshrc
 cp ~/.xinitrc ./xinitrc
+cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
 
 diff --brief -Nr ./hax ~/.hax > /dev/null || (rm -rf ./hax; cp -r ~/.hax ./hax)
 diff --brief -Nr ./i3 ~/config/i3 > /dev/null || (rm -rf ./i3; cp -r ~/.config/i3 ./i3)
