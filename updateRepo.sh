58486e0ca1795d6645f526455a5de7914bdd584d
Stanislav Ch. Nikolov <stanislav@cukii.me>
2019-03-09 20:45:11 +0200

    new updateRepo.sh

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,9 +1,10 @@
 #!/bin/env bash
 
-cp ~/.config/nvim/init.vim ./init.vim
+cp ~/.config/kak/kakrc ./kakrc
 cp ~/.config/zsh/zshrc ./zshrc
 cp ~/.xinitrc ./xinitrc
 cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
+cp ~/.autorun ~/autorun
 
 diff --brief -Nr ./hax ~/.hax > /dev/null || (rm -rf ./hax; cp -r ~/.hax ./hax)
 diff --brief -Nr ./i3 ~/config/i3 > /dev/null || (rm -rf ./i3; cp -r ~/.config/i3 ./i3)
